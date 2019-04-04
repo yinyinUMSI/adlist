f0d4f6d7705da74a2c76f93e79e70780884a40b6
yinyinUMSI <yinyin@umich.edu>
2019-04-04 12:35:58 +0000

    first commit

---
+++ ads/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from . import views
+from django.views.generic import TemplateView
+from django.urls import reverse_lazy
+
+urlpatterns = [
+    path('', views.AdListView.as_view()),
+    path('ads', views.AdListView.as_view(), name='ads'),
+    path('ad/<int:pk>', views.AdDetailView.as_view(), name='ad_detail'),
+    path('ad/create',
+        views.AdCreateView.as_view(success_url=reverse_lazy('ads')), name='ad_create'),
+    path('ad/<int:pk>/update',
+        views.AdUpdateView.as_view(success_url=reverse_lazy('ads')), name='ad_update'),
+    path('ad/<int:pk>/delete',
+        views.AdDeleteView.as_view(success_url=reverse_lazy('ads')), name='ad_delete'),
+]
